c5b87bdbfbbd26b76036955042410dd3c5435b38
Kusumlimb <kusumlimbu75@gmail.com>
2024-07-30 20:43:46 +0545

    second commit

---
+++ projectbrand/src/components/Hero.jsx
@@ -0,0 +1,28 @@
+const HeroSection = () => {
+    return <main className="hero">
+        <div className="hero-content">
+            <h1>YOUR FEET DESERVE THE BEST</h1>
+            <p>YOUR FEET DESERVE THE BEST AND WE ARE HERE TO HELP YOU WITH OUR SHOES</p>
+            <div className="hero-btn">
+                <button>Shop now</button>
+                <button>category</button>
+        
+            </div>
+            <div className="shopping">
+                <p>Also available on</p>
+                <div className="brand-icons">
+                    <img src="/images/flipkart.png" alt="shoe" />
+                </div>
+            </div>
+        </div>
+        <div className="hero-image">
+        <img src="/images/shoe.png" alt="hero-image" />
+        </div>
+
+
+
+
+    </main>
+};
+
+export default HeroSection;
